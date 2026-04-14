9f5b68d598f73b2e935c630f1c6d3bcb9e7d354c
Mouad <mouadse@gmail.com>
2026-04-14 11:20:07 +0100

    Minor modifcation to the dev CORS

--- api/middleware.go
+++ api/middleware.go
@@ -29,8 +29,10 @@ const (
 var defaultDevelopmentCORSOrigins = []string{
 	"http://localhost:3000",
 	"http://localhost:5173",
+	"http://localhost:5174",
 	"http://127.0.0.1:3000",
 	"http://127.0.0.1:5173",
+	"http://127.0.0.1:5174",
 }
 
 type corsConfig struct {
